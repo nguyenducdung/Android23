e3228a2ca00779c1bb83fc94f6d15b5e1ed59d34
hoangtu1234 <97843313+hoangtu1234@users.noreply.github.com>
2022-07-11 15:27:03 +0700

    btvn buoi2

--- app/src/main/java/com/dungnd/android23/CodeFresherActivity.java
+++ app/src/main/java/com/dungnd/android23/CodeFresherActivity.java
@@ -9,6 +9,6 @@ public class CodeFresherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_code_fresher);
+        setContentView(R.layout.buoi2_layout1);
     }
 }
